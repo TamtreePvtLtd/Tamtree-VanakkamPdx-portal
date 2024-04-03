894d92229983ebc9b2fb52aee99cbaa7e0d602d3
kiruthikaSukumar <kiruthikasukumar@gmail.com>
2024-04-03 09:48:22 +0530

    menucard

---
+++ src/seed-data/seed-data.ts
@@ -0,0 +1,13 @@
+export const dosaItems: IMenuCard[] = [
+  {
+    name: "Chicken Roast",
+    description: "Indian Style savory crepes served with Sambar & Chutney",
+    price: 9,
+  },
+  { name: "Sudha Plain Dosa", description: "", price: 9 },
+  { name: "Onion Dosa", description: "", price: 9 },
+  { name: "Onion Dosa", description: "", price: 9 },
+  { name: "Onion Dosa", description: "", price: 9 },
+  { name: "Onion Dosa", description: "", price: 9 },
+  { name: "Onion Dosa", description: "", price: 9 },
+ ];
