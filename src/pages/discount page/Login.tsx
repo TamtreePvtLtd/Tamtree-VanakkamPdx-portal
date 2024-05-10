6f02406d8862bb1d3c3600b0e2c255702c604b08
Logeswaran <logeswaran2108@gmail.com>
2024-05-10 11:48:54 +0530

    button color and dollar sign

--- src/pages/discount page/Login.tsx
+++ src/pages/discount page/Login.tsx
@@ -149,7 +149,6 @@ const handleClearForm = () => {
                 borderColor: "green",
                 color: "green",
                 "&:hover": {
-                  bgcolor: "white",
                   borderColor: "green",
                 },
               }}
